22a68d9cc17035c35034c6124e9fbe0f0304d5fe
LillyMS <lilly.marlene.siewert@ut.ee>
2024-11-07 15:46:16 +0200

    img, json (in store)

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,35 @@
 import { createStore } from 'vuex'
+import postsData from '../assets/posts.json'
 
 export default createStore({
   state: {
+    posts: postsData,
   },
   getters: {
+    posts: (state) => state.posts,
   },
   mutations: {
+    INCREMENT_LIKES(state, postId) {
+      const post = state.posts.find((post) => post.id === postId);
+      if (post) {
+        post.likes += 1;
+      }
+    },
+    
+    RESET_ALL_LIKES(state) {
+      state.posts.forEach((post) => {
+        post.likes = 0;
+      });
+    },
   },
   actions: {
+    likePost({ commit }, postId) {
+      commit('INCREMENT_LIKES', postId);
+    },
+
+    resetAllLikes({ commit }) {
+      commit('RESET_ALL_LIKES');
+    },
   },
   modules: {
   }

5a4e419cad74c68d851f7c024e6c15de2129205d
LillyMS <lilly.marlene.siewert@ut.ee>
2024-11-07 15:46:37 +0200

    posts component & view

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/addPost">Add Post</router-link>
   </nav>
   <router-view/>
 </template>
@@ -27,4 +27,20 @@ nav a {
 nav a.router-link-exact-active {
   color: #42b983;
 }
+
+:root {
+    --box-background-color: #ffffff;
+    --header-background-color: #b0c4e3;
+    --footer-background-color: #b0c4e3;
+    --hover-color: #678cc2;
+    --button-color: #1344d6;
+    --link-color: #1344d6;
+    --text-color: #080708;
+    --background-color: #e6e8e6;
+}
+
+h1 ~ a {
+    color: var(--link-color);
+}
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import PostsView from '@/views/PostsView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: PostsView
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/addPost',
+    name: 'addPost',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.

497b4b8f32e17940d6777d8d84c7cb1801841efb
LillyMS <lilly.marlene.siewert@ut.ee>
2024-11-08 15:53:44 +0200

    Add Post Component & View (#2)
    * img, json (in store)
    
    * posts component & view

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/addPost">Add Post</router-link>
   </nav>
   <router-view/>
   <footer-component/>
@@ -62,4 +62,9 @@ nav a.router-link-exact-active {
     --text-color: #080708;
     --background-color: #e6e8e6;
 }
+
+h1 ~ a {
+    color: var(--link-color);
+}
+
 </style>
---
+++ src/views/PostsView.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="posts" v-for="post in posts" :key="post.id">
+        <post-component :post="post"/>
+    </div>
+    <button @click="resetAllLikes">Reset likes</button>
+</template>
+
+<script>
+import PostComponent from '@/components/PostComponent.vue';
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+    name: "PostsView",
+    components: {
+        PostComponent,
+    },
+    computed: {
+        ...mapGetters(['posts'])
+        // alternative: 
+        // posts() {
+        //     return this.$store.getters.posts;
+        // },
+    },
+    methods: {
+        ...mapActions(['resetAllLikes'])
+        // alternative:
+        // resetLikes() {
+        //     this.$store.dispatch('resetAllLikes');
+        // },
+    }
+}
+
+</script>
+
+<style scoped>
+div.posts {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+button {
+    all: unset;
+    background-color: var(--button-color);
+    font-size: large;
+    color: white;
+    font-weight: bold;
+    border-radius: 25px;
+    width: 200px;
+    align-self: center;
+    padding: 10px;
+    margin-top: 10px;
+    margin-bottom: 20px;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, .1), 0px 8px 16px rgba(0, 0, 0, .1);
+}
+
+button:hover{
+    background-color: var(--hover-color);
+}
+</style>
